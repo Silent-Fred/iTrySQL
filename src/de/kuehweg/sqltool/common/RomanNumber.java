1fa1574e272212af40ae41b388fb0fddba0c1dc1
Michael Kühweg <michael@Michaels-iMac.local>
2013-10-07 19:43:55 +0200

    RomanNumber leicht überarbeitet

--- src/de/kuehweg/sqltool/common/RomanNumber.java
+++ src/de/kuehweg/sqltool/common/RomanNumber.java
@@ -39,18 +39,18 @@ public class RomanNumber {
 
     private enum Digit {
 
-        I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400),
-        D(500), CM(900), M(1000);
+        M(1000), CM(900), D(500), CD(400), C(100), XC(90), L(50), XL(40), X(10),
+        IX(9), V(5), IV(4), I(1);
         int value;
 
         private Digit(int value) {
             this.value = value;
         }
 
-        public static Digit fit(final long number) {
-            for (int i = values().length - 1; i >= 0; i--) {
-                if (values()[i].value <= number) {
-                    return values()[i];
+        public static Digit fit(final long number) throws IllegalArgumentException {
+            for (final Digit digit : values()) {
+                if (digit.value <= number) {
+                    return digit;
                 }
             }
             throw new IllegalArgumentException("Cannot fit " + number
@@ -60,13 +60,18 @@ public class RomanNumber {
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
-        long remainder = value;
-        while (remainder > 0) {
-            final Digit romanDigit = Digit.fit(remainder);
-            builder.append(romanDigit.name());
-            remainder -= romanDigit.value;
+        try {
+            StringBuilder builder = new StringBuilder();
+            long remainder = value;
+            Digit romanDigit;
+            do {
+                romanDigit = Digit.fit(remainder);
+                builder.append(romanDigit.name());
+                remainder -= romanDigit.value;
+            } while (remainder > 0);
+            return builder.toString();
+        } catch (IllegalArgumentException ex) {
+            return "mendum";
         }
-        return builder.toString();
     }
 }
