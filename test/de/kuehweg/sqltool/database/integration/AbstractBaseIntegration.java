c7ae2e8a93576c0380b0dffbf984515d9343cda3
Michael Kühweg <michael@Michaels-iMac.local>
2015-05-30 19:22:15 +0200

    Integrationstest

---
+++ test/de/kuehweg/sqltool/database/integration/AbstractBaseIntegration.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2015, Michael Kühweg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.kuehweg.sqltool.database.integration;
+
+import de.kuehweg.sqltool.database.DatabaseConstants;
+import de.kuehweg.sqltool.database.JDBCType;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Michael Kühweg
+ */
+public abstract class AbstractBaseIntegration {
+
+    private Connection connection;
+
+    protected Connection getTestConnection() {
+        return connection;
+    }
+    
+    protected void openTestConnection() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Class.forName(JDBCType.HSQL_IN_MEMORY.getDriverClass()).newInstance();
+        final Properties properties = new Properties();
+        properties.setProperty("user", "JOHN_DOE");
+        properties.setProperty("password", "secret");
+        properties.setProperty("shutdown", "true");
+        // Transaktionsmanagements wie in der Applikation verwenden
+        properties.setProperty(
+                DatabaseConstants.HSQLDB_PROPERTY_TX_CONTROL,
+                DatabaseConstants.DEFAULT_TRANSACTION_CONTROL);
+        connection = DriverManager.getConnection(JDBCType.HSQL_IN_MEMORY.getUrlPrefix() + "mem", properties);
+    }
+
+    protected void closeTestConnection() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    protected void dropSchema(String schema) throws SQLException {
+        if (connection != null) {
+            connection.createStatement().execute("DROP SCHEMA " + schema + " CASCADE");
+        }
+    }
+
+
+    protected void dropPublicSchema() throws SQLException {
+        dropSchema("PUBLIC");
+    }
+
+}
---
+++ test/de/kuehweg/sqltool/database/metadata/MetaDataReaderIntegrationTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2015, Michael Kühweg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.kuehweg.sqltool.database.metadata;
+
+import de.kuehweg.sqltool.database.integration.AbstractBaseIntegration;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Michael Kühweg
+ */
+public class MetaDataReaderIntegrationTest extends AbstractBaseIntegration {
+
+    public MetaDataReaderIntegrationTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        openTestConnection();
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        dropPublicSchema();
+        closeTestConnection();
+    }
+
+    @Test
+    public void readMetaData() throws SQLException {
+        DatabaseDescription db = new MetaDataReader().readMetaData(getTestConnection());
+        // Standardanmeldung: 1 Catalog mit 2 System-Schemas
+        Assert.assertEquals(1, db.getCatalogs().size());
+        Assert.assertEquals(2, db.getCatalogs().iterator().next().getSchemas().size());
+    }
+
+    @Test
+    public void createTableAndReadMetaData() throws SQLException {
+        getTestConnection().createStatement().execute(
+                "create table test (test numeric(10))");
+        DatabaseDescription db = new MetaDataReader().readMetaData(getTestConnection());
+        // Standardanmeldung: 1 Catalog mit 2 System-Schemas
+        // zusätzliches Benutzerschema
+        Assert.assertEquals(1, db.getCatalogs().size());
+        Assert.assertEquals(3, db.getCatalogs().iterator().next().getSchemas().size());
+        
+        for (SchemaDescription schema : db.getCatalogs().iterator().next().getSchemas()) {
+            if (schema.getSchema().equals("PUBLIC")) {
+                Assert.assertEquals(1, schema.getTables().size());
+                Assert.assertEquals("TEST", schema.getTables().iterator().next().getTableName());
+                Assert.assertEquals("TABLE", schema.getTables().iterator().next().getTableType());
+            }
+        }
+    }
+
+    @Test
+    public void createTableAndViewAndReadMetaData() throws SQLException {
+        getTestConnection().createStatement().execute(
+                "create table test_table (test numeric(10))");
+        getTestConnection().createStatement().execute(
+                "create view test_view as select * from test_table");
+        DatabaseDescription db = new MetaDataReader().readMetaData(getTestConnection());
+        // Standardanmeldung: 1 Catalog mit 2 System-Schemas
+        // zusätzliches Benutzerschema
+        Assert.assertEquals(1, db.getCatalogs().size());
+        Assert.assertEquals(3, db.getCatalogs().iterator().next().getSchemas().size());
+        
+        for (SchemaDescription schema : db.getCatalogs().iterator().next().getSchemas()) {
+            if (schema.getSchema().equals("PUBLIC")) {
+                Assert.assertEquals(2, schema.getTables().size());
+                Assert.assertEquals(1, schema.getTablesByType("TABLE").size());
+                Assert.assertEquals(1, schema.getTablesByType("VIEW").size());
+            }
+        }
+    }
+
+}
